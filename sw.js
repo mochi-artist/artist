a64f10a8044d6d6d3772507f3472b89522692711
art <a131917577@gmail.com>
2026-03-01 12:03:34 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,56 @@
-const CACHE_NAME = 'railway-app-v1';
-// 這裡放您網站一定要載入的基礎檔案
+// ★ 核心版本號 v3
+const CACHE_NAME = 'railway-app-v3'; 
+
+// 預先下載清單：包含首頁、所有畫圖工具(JS)與靜態圖片
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
   './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icons/icon-512.png',
+  './diagram_output.html', 
+  './lines.html',
+  './navbar.html',
+  './css/web.css',
+  
+  // JS 畫圖工具
+  './js/config.js',
+  './js/diagram.js',
+  './js/diagram_output.js',
+  './js/time_space.js',
+  './js/timer.js',
+  './js/url.min.js',
+  './js/util.js',
+
+  // 靜態圖片
+  './images/browser.png',
+  './images/facebook.png',
+  './images/github.png',
+  './images/gmail.png',
+  './images/instagram.png',
+  './images/timespan.png'
 ];
 
-// 安裝階段：快取基礎檔案
+// 安裝階段：將上面的清單強制存入手機
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('正在預先下載核心檔案...');
+        return cache.addAll(urlsToCache);
+      })
       .then(() => self.skipWaiting())
   );
 });
 
-// 啟動階段：清理舊快取
+// 啟動階段：清理舊版本的快取
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cache => {
           if (cache !== CACHE_NAME) {
+            console.log('清除舊快取:', cache);
             return caches.delete(cache);
           }
         })
@@ -32,12 +59,12 @@ self.addEventListener('activate', event => {
   );
 });
 
-// 攔截請求：網路優先 (Network First)
+// 攔截請求：網路優先 (Network First) + 動態收集資料
 self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request)
       .then(response => {
-        // 有網路就順便更新快取
+        // 【動態收集】不管抓到什麼(包括資料檔)，都順手存進倉庫
         const clonedResponse = response.clone();
         caches.open(CACHE_NAME).then(cache => {
           cache.put(event.request, clonedResponse);
@@ -45,8 +72,8 @@ self.addEventListener('fetch', event => {
         return response;
       })
       .catch(() => {
-        // 沒網路就讀取快取
-        return caches.match(event.request);
+        // 【斷網讀取】沒網路時從倉庫拿資料，無視網址的問號參數
+        return caches.match(event.request, { ignoreSearch: true });
       })
   );
 });
